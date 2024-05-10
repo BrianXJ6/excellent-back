048a621cf0a874930534e765eeb56ca2b93e04ed
Brian Barros <brian.ferreira@pie.tax>
2024-05-09 22:30:12 -0300

    Refactor user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,9 +2,8 @@
 
 namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -13,6 +12,8 @@ class UserFactory extends Factory
 {
     /**
      * The current password being used by the factory.
+     *
+     * @var null|string
      */
     protected static ?string $password;
 
@@ -27,13 +28,14 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'password' => static::$password ??= Hash::make('1234567890'),
         ];
     }
 
     /**
      * Indicate that the model's email address should be unverified.
+     *
+     * @return static
      */
     public function unverified(): static
     {
