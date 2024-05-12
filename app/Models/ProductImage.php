298371fc8f0c9eebdcba86fbcfd8f0e5b277c20e
Brian Barros <brianferreira15@hotmail.com>
2024-05-12 17:12:38 -0300

    Accessor for path in Product Images model

--- app/Models/ProductImage.php
+++ app/Models/ProductImage.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use App\Support\ORM\BaseSimpleModel;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ProductImage extends BaseSimpleModel
@@ -20,6 +22,27 @@ class ProductImage extends BaseSimpleModel
      * @var array<int, string>
      */
     protected $fillable = ['path'];
+
+    /**
+     * Get the path's image.
+     *
+     * @return \Illuminate\Database\Eloquent\Casts\Attribute
+     */
+    protected function path(): Attribute
+    {
+        return Attribute::make(
+            get: function (?string $path) {
+                if (!$path) {
+                    return null;
+                }
+
+                /** @var \Illuminate\Filesystem\Filesystem */
+                $disk = Storage::disk('public');
+
+                return $disk->url($path);
+            },
+        );
+    }
 
     /**
      * Get the product that owns the image.
